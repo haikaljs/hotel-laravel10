b382c3edb68edd9841127ac87cffd147e5428ecb
haikaljs <haikaljavascript@gmail.com>
2023-10-07 04:01:04 +0800

    Seed demo user data

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            // Admin
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'admin',
+                'status' => 'active'
+            ],
+
+            // User
+            [
+                'name' => 'User',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'user',
+                'status' => 'active'
+            ],
+
+        ]);
+    }
+}
