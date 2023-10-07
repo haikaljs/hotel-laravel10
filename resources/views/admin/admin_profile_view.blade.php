120871b63d906beb5c524582a30e0f97456b5400
haikaljs <haikaljavascript@gmail.com>
2023-10-08 05:27:47 +0800

    Admin profile image update part3

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -1,5 +1,8 @@
 @extends('admin.admin_dashboard')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+
 <div class="page-content">
     <!--breadcrumb-->
     <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
@@ -99,7 +102,7 @@
                                     <h6 class="mb-0">Photo</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input class="form-control" name="photo" type="file" id="formFile">
+                                    <input class="form-control" name="photo" type="file" id="image">
                                 </div>
                             </div>
 
@@ -108,7 +111,7 @@
                                     <h6 class="mb-0">Photo</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <img src=" {{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg')  }}" alt="Admin" class="rounded-circle p-1 bg-primary" width="60">
+                                    <img id="showImage" src=" {{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg')  }}" alt="Admin" class="rounded-circle p-1 bg-primary" width="80">
                                 </div>
                             </div>
 
@@ -127,4 +130,19 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+
+$(document).ready(function(){
+    $('#image').change(function(e){
+        var reader = new FileReader()
+        reader.onload = function(e){
+            $('#showImage').attr('src', e.target.result)
+        }
+        reader.readAsDataURL(e.target.files['0'])
+
+    })
+})
+
+</script>
 @endsection
