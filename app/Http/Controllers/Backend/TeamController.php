3d7fe93e004dd2bf20c869ec27fd60b1c170ca3b
haikaljs <haikaljavascript@gmail.com>
2023-10-09 07:49:29 +0800

    Team setup part2

--- app/Http/Controllers/Backend/TeamController.php
+++ app/Http/Controllers/Backend/TeamController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Team;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TeamController extends Controller
 {
     public function AllTeam(){
-        
+        $team = Team::latest()->get(); 
+        return view('backend.team.all_team', compact('team'));
     }
 }
--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -29,6 +29,9 @@
 
 	{{-- toaster css--}}
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+
+	{{-- datatable css--}}
+	<link href="{{ asset('backend/assets/plugins/datatable/css/dataTables.bootstrap5.min.css') }}" rel="stylesheet" />
 	<title>Admin Dashboard</title>
 </head>
 
@@ -109,6 +112,15 @@
     break; 
  }
  @endif 
+</script>
+
+{{-- datatable --}}
+<script src=" {{ asset('backend/assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('backend/assets/plugins/datatable/js/dataTables.bootstrap5.min.js') }}"></script>
+<script>
+	$(document).ready(function() {
+		$('#example').DataTable();
+	  } );
 </script>
 </body>
 
